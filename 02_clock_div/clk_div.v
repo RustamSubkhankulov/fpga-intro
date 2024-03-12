bb41d16203652c4c4ae7800f03664972613c68ca
Rustam Subkhankulov <subkhankulov@ispras.ru>
2024-03-12 18:06:17 +0300

    02_clock_div: done

---
+++ 02_clock_div/clk_div.v
@@ -0,0 +1,19 @@
+module clk_div #(parameter X = 16)(
+  input wire clk,
+
+  output wire clk_divided
+);
+
+reg [$clog2(X):0]cnt = 0;
+assign clk_divided = (cnt == X);
+
+always @(posedge clk) begin
+  cnt <= cnt + 1;
+end
+
+always @(negedge clk) begin
+  if (clk_divided)
+    cnt <= 0;
+end
+
+endmodule
---
+++ 02_clock_div/testbench.v
@@ -0,0 +1,29 @@
+`timescale 1 ns / 100 ps
+
+module testbench();
+
+/* Represents clock, initial value is 0 */
+reg clk = 1'b0;
+
+/* Toggle clock every 1ns */
+always begin
+    #1 clk = ~clk;
+end
+
+wire clk_divided;
+
+clk_div #(.X(8)) clk_div(.clk(clk), .clk_divided(clk_divided));
+
+initial begin
+    
+    /* Open for dump of signals */
+    $dumpvars;
+    
+    /* Write to console */
+    $display("Test started...");
+    
+    /* Stop simulation after 64ns */
+    #64 $finish;
+end
+
+endmodule
