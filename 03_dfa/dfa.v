06f90c199d74df2036fd851235972d398488be0e
Rustam Subkhankulov <subkhankulov@ispras.ru>
2024-03-13 01:59:40 +0300

    implemented dfa using verilog

--- 03_dfa/dfa.v
+++ 03_dfa/dfa.v
@@ -1,5 +1,45 @@
-module dfa();
+module dfa(
+  
+  /* Clocking signal */
+  input wire clk,
 
+  /* Signals for 1,2 and 5 coins inserted */
+  input wire in1,
+  input wire in2,
+  input wire in5,
 
+  /* Output for change: 1, 2 and 2 times 2 coins */
+  output wire out1,
+  output wire out2,
+  output wire out2x2,
+
+  /* Output for soda */
+  output wire soda
+);
+
+/* Current state */
+reg [2:0]S = 0;
+
+assign out1 = (S[0] & in5) | (S[2] & in2);
+assign out2 = S[1] & in5;
+assign out2x2 = S[2] & in5;
+
+assign soda = in5 | (S[2] & (in1|in2)) | S[0] & S[1] & in2; 
+
+always @(posedge clk) begin
+
+  S[0] <= (~S[2] & ~S[0] &  in1) 
+        | (~S[1] &  S[0] & ~in1 & ~in5) 
+        | ( S[1] &  S[0] & ~in1 & ~in2 & ~in5);
+
+  S[1] <= (~S[2] & ~S[1] &  in2)
+        | (~S[1] &  S[0] &  in1 & ~in2 & ~in5)
+        | (S[1]  & ~S[0] & ~in2 & ~in5)
+        | (S[1]  &  S[0] & ~in1 & ~in2 & ~in5);
+
+  S[2] <= (S[1] & ~S[0]  & in2)
+        | (S[1] &  S[0]  & in1)
+        | (S[2] & ~in1 & ~in2 & ~in5);
+end
 
 endmodule
--- 03_dfa/testbench.v
+++ 03_dfa/testbench.v
@@ -11,8 +11,20 @@ always begin
     #1 clk = ~clk;
 end
 
+/* Input signals for dfa */
+reg in1 = 1'b0;
+reg in2 = 1'b0;
+reg in5 = 1'b0;
+
+/* Wires for output signals of dfa */
+wire out1;
+wire out2;
+wire out2x2;
+wire soda;
+
 /* Create instance of dfa module */
-dfa dfa();
+dfa dfa(.clk(clk), .in1(in1), .in2(in2), .in5(in5), 
+        .out1(out1), .out2(out2), .out2x2(out2x2), .soda(soda));
 
 initial begin
     /* Open for dump of signals */
@@ -21,8 +33,8 @@ initial begin
     /* Write to console */
     $display("Test started...");
 
-    /* Stop simulation after 64ns */
-    #64 $finish;
+    /* Stop simulation after 10ns */
+    #10 $finish;
 end
 
 endmodule
